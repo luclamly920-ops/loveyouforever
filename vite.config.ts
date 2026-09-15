8cd4f75b12cec44871e5c17abaf5f84e8276c8f6
luclamly920-ops <luclamly920@gmail.com>
2026-09-15 18:01:33 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/loveyouforever/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
